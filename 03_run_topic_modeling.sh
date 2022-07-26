c26658c04263a53a6e5bbac08e87f1d257a06cf5
migrationsKB <migrationsKB@protonmail.com>
2022-07-26 11:03:14 +0200

    run topic modeling script

--- 03_run_topic_modeling.sh
+++ 03_run_topic_modeling.sh
@@ -3,7 +3,7 @@
 # 3.1 build data for inferring topic modeling
 # param: lang_code
 # define the language code
-python -m models.topicModeling.ETModel.data_build_for_inferring_topics --lang_code el
+python -m models.topicModeling.ETM.data_build_for_inferring_topics --lang_code el
 # the data would be stored in output/preprocessed/forTP/lang_code/..
 
 
